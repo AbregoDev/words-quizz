d6540bdee91cf9403c2464d22f176a2c3a02624f
Daniel Moreno <daniel.ingeniero@outlook.com>
2025-06-29 23:04:46 -0600

    config

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,7 +1,7 @@
 import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'io.ionic.starter',
+  appId: 'dev.abrego.wordsquizz',
   appName: 'words-quizz',
   webDir: 'www'
 };
