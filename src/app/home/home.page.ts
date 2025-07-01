fd31756cc3b8d72052feade3438408d8d908f4dc
Daniel Moreno <daniel.ingeniero@outlook.com>
2025-06-30 20:32:08 -0600

    feat: initial quizz template

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,51 @@
 import { Component } from '@angular/core';
-import { IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/angular/standalone';
+import {
+  IonHeader,
+  IonToolbar,
+  IonTitle,
+  IonContent,
+  IonText,
+  IonButton,
+  IonFooter,
+  IonProgressBar,
+} from '@ionic/angular/standalone';
+
+interface Option {
+  text: string;
+  isCorrectoAnswer: boolean;
+}
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
-  imports: [IonHeader, IonToolbar, IonTitle, IonContent],
+  imports: [
+    IonHeader,
+    IonToolbar,
+    IonTitle,
+    IonContent,
+    IonText,
+    IonButton,
+    IonFooter,
+    IonProgressBar,
+  ],
 })
 export class HomePage {
+
+  options: Option[] = [
+    {
+      text: 'Divulgar algo oculto que generalmente se considera negativo',
+      isCorrectoAnswer: false,
+    },
+    {
+      text: 'Anular, tachar o borrar',
+      isCorrectoAnswer: true,
+    },
+    {
+      text: 'Que implica o denota duda',
+      isCorrectoAnswer: false,
+    },
+  ];
+
   constructor() {}
 }
